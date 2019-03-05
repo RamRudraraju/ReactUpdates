c8487fbf6c50241f3e50254f49da2781425d2643
Rama Rudraraju <rama.rudraraju@vistex.com>
2019-03-05 00:33:46 -0600

    Rendering Basic Example

---
+++ ReactDOM_Rendering.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+               <div> <ul>
+                  <li> India </li>
+                  <li> America </li>
+                  <li> China </li>
+                </ul>
+                </div>);
+
+ReactDOM.render(myList, document.getElementById("app") );
