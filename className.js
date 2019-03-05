19d68613d4a968d8ff92fce04a5393d49e1edc34
Rama krishna Raju Rudraraju <rrkr2016@uab.edu>
2019-03-05 11:02:59 -0600

    Update className.js

--- className.js
+++ className.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Write code here:
+// In JSX, you can't use the word class! You have to use className instead
+// When JSX is rendered, JSX className attributes are automatically rendered as class attributes.
 const myDiv = (<div className="big">I AM A BIG DIV</div>);
 
 ReactDOM.render(myDiv, document.getElementById('app'));
