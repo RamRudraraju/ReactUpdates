b6349ffda11b08a39cdcd039d5728824b4f54644
Rama krishna Raju Rudraraju <rrkr2016@uab.edu>
2019-06-28 08:10:40 -0500

    Add files via upload

---
+++ Components/Comp Rendering/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class NavBar extends React.Component {
+  render() {
+    const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+    const navLinks = pages.map(page => {
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      )
+    });
+   const x = <nav>{navLinks}</nav>;
+    return x;
+  }
+}
